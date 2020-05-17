8ec5f42807676773d2266841606ee546e29a353d
bencrismanich <bencrismanich@gmail.com>
2020-05-17 15:12:41 +1000

    Adding in AZ b, extra resources: NAT gateway, internet gateway, EIP, EIP association, database subnet

--- asg_example.tf
+++ asg_example.tf
@@ -1,23 +1,23 @@
 provider "aws" {
   profile = "default"
-  region = var.region
+  region = "${var.region}"
 }
 
-resource "aws_launch_template" "asg_example" {
-  name          = "asg_example"
-  image_id      = "ami-2757f631"
-  instance_type = "t2.micro"
-}
+# resource "aws_launch_template" "asg_example" {
+#   name          = "asg_example"
+#   image_id      = "ami-2757f631"
+#   instance_type = "t2.micro"
+# }
 
-resource "aws_autoscaling_group" "bc-asg-sandpit" {
-name = "bc-asg-sandpit"
-availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
+# resource "aws_autoscaling_group" "bc-asg-sandpit" {
+# name = "bc-asg-sandpit"
+# availability_zones = ["us-east-1a"]
+#   desired_capacity   = 1
+#   max_size           = 1
+#   min_size           = 1
 
-  launch_template {
-      id = "${aws_launch_template.asg_example.id}"
-      version = "$Latest"
-  }
-}
+#   launch_template {
+#       id = "${aws_launch_template.asg_example.id}"
+#       version = "$Latest"
+#   }
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,34 +1,117 @@
-
-# Create VPC
+# Create VPC with 65,534 available addresses
 resource "aws_vpc" "wp-vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support = true
-
-
     tags = {
         Name = "wp-vpc"
     }
 }
 
-# Create public subnet
-resource "aws_subnet" "public-subnet-1a" {
-    vpc_id = "${aws_vpc.wp-vpc.vpc_id}"
-    cidr_blocks = "10.0.0.0/24"
+##############################################################
+#################### SUBNETS - us-east-1a ####################
+##############################################################
+
+# Carve up /16 to create public subnet with /24
+resource "aws_subnet" "public-global-1a" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    cidr_block = "10.0.0.0/24"
     availability_zone = "us-east-1a"
 
     tags = {
-        Name = "Public Subnet"
+        Name = "Public-AZ-A"
     }
 }
 
-# Create private subnet
-resource "aws_subnet" "private-subnet-1a" {
-    vpc_id = "${aws_vpc.wp-vpc.vpc_id}"
-    cidr_blocks = "10.0.1.0/24"
+# /24 subnet for web applications
+resource "aws_subnet" "private-app-1a" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
 
     tags = {
-        Name = "Private Subnet - Application Servers"
+        Name = "App-AZ-A"
+    }
+}
+
+# /24 subnet for database applications
+resource "aws_subnet" "private-data-1a" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "Data-AZ-A"
+    }
+}
+
+##############################################################
+#################### SUBNETS - us-east-1a ####################
+##############################################################
+
+# Carve up /16 to create public subnet with /24
+resource "aws_subnet" "public-global-1b" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "Public-AZ-B"
+    }
+}
+
+# /24 subnet for web applications
+resource "aws_subnet" "private-app-1b" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    cidr_block = "10.0.4.0/24"
+    availability_zone = "us-east-1b"
+
+    tags = {
+        Name = "App-AZ-B"
+    }
+}
+
+# /24 subnet for database applications
+resource "aws_subnet" "private-data-1b" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    cidr_block = "10.0.5.0/24"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "Data-AZ-B"
+    }
+}
+
+##############################################################
+##############################################################
+
+resource "aws_eip_association" "nat-eip-association-1b" {
+    instance_id = "${aws_nat_gateway.nat-gateway-1b.id}"
+    allocation_id = "${aws_eip.nat-eip-1b.id}"
+    tags = {
+        Name = "NAT-EIP Association 1a"
+    }
+}
+
+# Create Elastic IP address and associate with NAT Gateway - 1b
+resource "aws_eip" "nat-eip-1b" {
+    vpc = true
+    tags = {
+        Name = "elastic-ip-1b"
+    }
+}
+
+# Create NAT Gateway with Elastic IP address - 1b
+resource "aws_nat_gateway" "nat-gateway-1b" {
+    allocation_id = "${aws_eip.nat-eip-1b.id}"
+    subnet_id = "${aws_subnet.public-global-1b.id}"
+    tags = {
+        Name = "NAT Gateway 1b"
+    }
+}
+
+# Create Internet Gateway - 1b
+resource "aws_internet_gateway" "internet-gateway-1b" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    tags = {
+        Name = "Internet Gateway 1b"
     }
 }
