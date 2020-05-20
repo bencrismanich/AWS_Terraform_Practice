ee3ced7cc9e443747075dac84bac5ee6d31eaba9
bencrismanich <bencrismanich@gmail.com>
2020-05-20 13:41:17 +1000

    Finalized application load balance, added database security group and sample ec2 instance

--- sg.tf
+++ sg.tf
@@ -21,7 +21,6 @@ resource "aws_security_group" "external-web-sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
     tags = {
         "Name" = "External Web Security Group"
     }
@@ -49,9 +48,31 @@ resource "aws_security_group" "internal-web-sg" {
         protocol = "tcp"
         security_groups = ["${aws_security_group.external-web-sg.id}"]
     }
-
     tags = {
         "Name" = "Internal Web Security Group"
     }
   
 }
+
+resource "aws_security_group" "internal-data-sg" {
+    vpc_id = "${aws_vpc.wp-vpc.id}"
+    description = "Internal Data Security Group"
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+        security_groups = ["${aws_security_group.internal-web-sg.id}"]
+    }
+    tags = {
+        "Name" = "Internal Data Security Group"
+    }
+  
+}
+
+
